5e2b06cd3ea7cb590d8b5c27edb4d4ff51e8755f
Jarosław Frydrych <jarek.frydrych@gmail.com>
2016-12-19 22:55:29 +0100

    addeded express with gulp

---
+++ server.conf.js
@@ -0,0 +1,88 @@
+// ```
+// server.conf.js
+// (c) 2016 David Newman
+// david.r.niciforovic@gmail.com
+// server.conf.js may be freely distributed under the MIT license
+// ```
+
+// *server.conf.js*
+
+//  This is the file where we will:
+//  - Configure our application
+//  - Connect to our database
+//  - Create our Mongoose models
+//  - Define routes for our RESTful API
+//  - Define routes for our frontend Angular application
+//  - Set the app to listen on a port so we can view it in our browser
+
+// # Node Env Variables
+
+// Load Node environment variable configuration file
+
+// # Modules
+
+// Load Express
+import express from 'express';
+// Load Node http module
+import http from 'http';
+// Create our app with Express
+let app = express();
+// Create a Node server for our Express app
+let server = http.createServer(app);
+// Log requests to the console (Express 4)
+import morgan from 'morgan';
+// Pull information from HTML POST (express 4)
+import bodyParser from 'body-parser';
+// Simulate DELETE and PUT (Express 4)
+import methodOverride from 'method-override';
+// PassportJS
+import passport from 'passport';
+import cookieParser from 'cookie-parser';
+
+import passportJWT from 'passport-jwt';
+import jwt from 'jsonwebtoken';
+
+// # Configuration
+
+// Set the port for this app
+let port = process.env.PORT || 8080;
+
+if (process.env.NODE_ENV === 'development' ||
+  process.env.NODE_ENV === 'test')
+// Log every request to the console
+  app.use(morgan('dev'));
+
+// Read cookies (needed for authentication)
+app.use(cookieParser());
+
+// ## Get all data/stuff of the body (POST) parameters
+
+// Parse application/json
+app.use(bodyParser.json());
+// Parse application/vnd.api+json as json
+app.use(bodyParser.json({type: 'application/vnd.api+json'}));
+// Parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Override with the X-HTTP-Method-Override header in the request. Simulate DELETE/PUT
+app.use(methodOverride('X-HTTP-Method-Override'));
+// Set the static files location /public/img will be /img for users
+app.use(express.static(__dirname + '/dist'));
+
+// ## Passport JS
+
+app.get('*', (req, res) => {
+
+  // Load our src/app.html file
+  //** Note that the root is set to the parent of this folder, ie the app root **
+  res.sendFile('/dist/index.html', {root: __dirname + "/../"});
+});
+
+
+server.listen(port);
+
+// Shoutout to the user
+console.log(`Wizardry is afoot on port ${port}`);
+
+// Expose app
+export {app};
