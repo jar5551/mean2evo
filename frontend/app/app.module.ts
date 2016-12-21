f5242b59ef8ab4b69f4690a929974c8dba273a9f
Jarosław Frydrych <jarek.frydrych@gmail.com>
2016-12-21 16:51:11 +0100

    implemented materialdesing but not fully working

--- frontend/app/app.module.ts
+++ frontend/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { PostsComponent } from './components/posts/posts.component';
@@ -18,7 +19,8 @@ import { PostsComponent } from './components/posts/posts.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    //routing
+    MaterialModule.forRoot()
+    //routing //TODO implement routing. Consider ui router or angykar routing
   ],
   providers: [],
   bootstrap: [AppComponent]
