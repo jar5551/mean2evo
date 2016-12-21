ca916ccca5df0e842dbfe59426536e92ddb5d636
Jarosław Frydrych <jarek.frydrych@gmail.com>
2016-12-21 16:35:26 +0100

    update metadata of app

--- backend/models/post.model.js
+++ backend/models/post.model.js
@@ -1,3 +1,13 @@
 /**
  * Created by jarek on 19/12/2016.
  */
+import mongoose from 'mongoose';
+
+let postSchema = new mongoose.Schema({
+  title: { type: String },
+  content: { type : String }
+});
+
+// Expose the model so that it can be imported and used in
+// the controller (to search, delete, etc.)
+export default mongoose.model('Post', postSchema);
--- backend/routes/post.router.js
+++ backend/routes/post.router.js
@@ -7,15 +7,35 @@ import Post from './../models/post.model';
 export default (app, router) => {
   router.route('/posts')
     .get((req, res) => {
-      var posts = [];
+      Post.find((err, posts) => {
+        if (err)
+          res.send(err);
 
-      for(var i = 0; i<20; i++) {
+        else
+          res.json(posts);
+      });
+    })
+
+    .post((req, res) => {
+      let posts = [];
+
+      for (var i = 0; i < 20; i++) {
         posts.push({
           title: 'Post ' + i,
           content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et erat elementum, fermentum diam in, blandit est. Pellentesque viverra, urna ut tempus blandit, neque felis sagittis massa, eget dignissim dolor leo at metus.'
         })
       }
 
-      res.json(posts);
+      posts.forEach(function (element) {
+        Post.create({
+          title: element.title,
+          content: element.content
+        }, (err, todo) => {
+          if (err)
+            res.send(err);
+
+          console.log(`Todo created: ${todo}`);
+        });
+      });
     });
 }
