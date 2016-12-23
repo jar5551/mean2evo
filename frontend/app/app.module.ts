65d3cfcef84e2cd27f3899e365d53f8041a7140d
Jarosław Frydrych <jarek.frydrych@gmail.com>
2016-12-23 01:22:27 +0100

    disabled ui router because of some issues

--- frontend/app/app.module.ts
+++ frontend/app/app.module.ts
@@ -1,19 +1,19 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import {HttpModule} from '@angular/http';
-import {MaterialModule} from '@angular/material';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
 import {UIRouterModule} from "ui-router-ng2";
 
-import {AppComponent} from './app.component';
-import {PostsComponent} from './components/posts/posts.component';
+import { AppComponent } from './app.component';
+import { PostsComponent } from './components/posts/posts.component';
 
 import {routing} from './app.routing';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostsComponent,
+    PostsComponent
     //routedComponents
   ],
   imports: [
@@ -22,10 +22,9 @@ import {routing} from './app.routing';
     HttpModule,
     MaterialModule.forRoot(),
     //routing //TODO implement routing. Consider ui router or angular routing
-    UIRouterModule.forRoot(routing)
+    //UIRouterModule.forRoot(routing)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule { }
