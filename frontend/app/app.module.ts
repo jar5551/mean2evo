7d1cbcc559793aabc9a08614ec8a709ebfdd4e65
Jarosław Frydrych <jarek.frydrych@gmail.com>
2016-12-23 01:06:43 +0100

    material desing, ui router and readme updates

--- frontend/app/app.module.ts
+++ frontend/app/app.module.ts
@@ -1,13 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { MaterialModule } from '@angular/material';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {MaterialModule} from '@angular/material';
+import {UIRouterModule} from "ui-router-ng2";
 
-import { AppComponent } from './app.component';
-import { PostsComponent } from './components/posts/posts.component';
+import {AppComponent} from './app.component';
+import {PostsComponent} from './components/posts/posts.component';
 
-//import {routing, routedComponents} from './app.routing';
+import {routing} from './app.routing';
 
 @NgModule({
   declarations: [
@@ -19,10 +20,12 @@ import { PostsComponent } from './components/posts/posts.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    MaterialModule.forRoot()
-    //routing //TODO implement routing. Consider ui router or angykar routing
+    MaterialModule.forRoot(),
+    //routing //TODO implement routing. Consider ui router or angular routing
+    UIRouterModule.forRoot(routing)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
