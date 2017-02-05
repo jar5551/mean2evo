9e5dd76be47494c51d09088128a7c6ff71acbd17
Jarosław Frydrych <jarek.frydrych@gmail.com>
2017-02-06 00:18:09 +0100

    remove smart tables because of error

--- app/app/admin/components/admin-posts/admin-posts.component.ts
+++ app/app/admin/components/admin-posts/admin-posts.component.ts
@@ -4,9 +4,6 @@ import {MdDialog, MdSnackBar} from '@angular/material';
 import {AdminPostsFormComponent} from './admin-posts-form.component';
 import {LoadingService} from './../../../components/loading-indicator/loading.service';
 
-import { SmartTablesService } from './smartTables.service';
-import { LocalDataSource } from 'ng2-smart-table';
-
 @Component({
   selector: 'app-admin-posts',
   templateUrl: './admin-posts.component.html',
@@ -18,57 +15,6 @@ export class AdminPostsComponent implements OnInit {
 
   public items: Array<any>;
   public templateConfig: Object;
-
-  //smart tables
-  query: string = '';
-
-  settings = {
-    add: {
-      addButtonContent: '<i class="ion-ios-plus-outline"></i>',
-      createButtonContent: '<i class="ion-checkmark"></i>',
-      cancelButtonContent: '<i class="ion-close"></i>',
-    },
-    edit: {
-      editButtonContent: '<i class="ion-edit"></i>',
-      saveButtonContent: '<i class="ion-checkmark"></i>',
-      cancelButtonContent: '<i class="ion-close"></i>',
-    },
-    delete: {
-      deleteButtonContent: '<i class="ion-trash-a"></i>',
-      confirmDelete: true
-    },
-    columns: {
-      id: {
-        title: 'ID',
-        type: 'number'
-      },
-      firstName: {
-        title: 'First Name',
-        type: 'string'
-      },
-      lastName: {
-        title: 'Last Name',
-        type: 'string'
-      },
-      username: {
-        title: 'Username',
-        type: 'string'
-      },
-      email: {
-        title: 'E-mail',
-        type: 'string'
-      },
-      age: {
-        title: 'Age',
-        type: 'number'
-      }
-    }
-  };
-
-  source: LocalDataSource = new LocalDataSource();
-
-  //smart tables end
-
 
 
   constructor(private adminPostsService: AdminPostsService,
